e0d4325a7120afd316dee8ba3b9d1d24a08e9bee
Felipe Coury <felipe.coury@gmail.com>
2026-01-02 00:49:31 -0300

    feat: scrollbar_corner_color example

---
+++ examples/scrollbar_corner_color.rs
@@ -0,0 +1,52 @@
+use textual::{App, Compose, KeyCode, Label, Widget, ui};
+
+const TEXT: &str = r#"I must not fear.
+Fear is the mind-killer.
+Fear is the little-death that brings total obliteration.
+I will face my fear.
+I will permit it to pass over me and through me.
+And when it has gone past, I will turn the inner eye to see its path.
+Where the fear has gone there will be nothing. Only I will remain.
+"#;
+
+#[derive(Clone)]
+enum Message {}
+
+struct ScrollbarCornerColorApp {
+    quit: bool,
+}
+
+impl ScrollbarCornerColorApp {
+    fn new() -> Self {
+        Self { quit: false }
+    }
+}
+
+impl Compose for ScrollbarCornerColorApp {
+    type Message = Message;
+
+    fn compose(&self) -> Vec<Box<dyn Widget<Self::Message>>> {
+        ui! {
+            Label(format!("{}\n{}", TEXT.replace("\n", " "), TEXT.repeat(10)))
+        }
+    }
+}
+
+impl App for ScrollbarCornerColorApp {
+    const CSS: &'static str = include_str!("scrollbar_corner_color.tcss");
+
+    fn on_key(&mut self, key: textual::KeyCode) {
+        if key == KeyCode::Char('q') || key == KeyCode::Esc {
+            self.quit = true;
+        }
+    }
+
+    fn should_quit(&self) -> bool {
+        self.quit
+    }
+}
+
+fn main() -> textual::Result<()> {
+    let mut app = ScrollbarCornerColorApp::new();
+    app.run()
+}
