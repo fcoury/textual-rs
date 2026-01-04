8a4fed7e1db82eee41f8ed8278dd77b669db0cea
Felipe Coury <felipe.coury@gmail.com>
2026-01-04 14:43:15 -0300

    feat: text_opacity, text_style, text_style_all, text_wrap, visibility

--- examples/visibility.rs
+++ examples/visibility.rs
@@ -3,32 +3,30 @@ use textual::{App, Compose, KeyCode, Label, Widget, ui};
 #[derive(Clone)]
 enum Message {}
 
-struct TextOpacityApp {
+struct WrapApp {
     quit: bool,
 }
 
-impl TextOpacityApp {
+impl WrapApp {
     fn new() -> Self {
         Self { quit: false }
     }
 }
 
-impl Compose for TextOpacityApp {
+impl Compose for WrapApp {
     type Message = Message;
 
     fn compose(&self) -> Vec<Box<dyn Widget<Self::Message>>> {
         ui! {
-            Label("text-opacity: 0%", id: "zero-opacity")
-            Label("text-opacity: 25%", id: "quarter-opacity")
-            Label("text-opacity: 50%", id: "half-opacity")
-            Label("text-opacity: 75%", id: "three-quarter-opacity")
-            Label("text-opacity: 100%", id: "full-opacity")
+            Label("Widget 1")
+            Label("Widget 2", classes: "invisible")
+            Label("Widget 3")
         }
     }
 }
 
-impl App for TextOpacityApp {
-    const CSS: &'static str = include_str!("text_opacity.tcss");
+impl App for WrapApp {
+    const CSS: &'static str = include_str!("visibility.tcss");
 
     fn on_key(&mut self, key: textual::KeyCode) {
         if key == KeyCode::Char('q') || key == KeyCode::Esc {
@@ -42,6 +40,6 @@ impl App for TextOpacityApp {
 }
 
 fn main() -> textual::Result<()> {
-    let mut app = TextOpacityApp::new();
+    let mut app = WrapApp::new();
     app.run()
 }
